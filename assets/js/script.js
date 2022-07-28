a3bc254fccf69b449c92340b139287526aff4c60
kkmusau <kkmusau@gmail.com>
2022-07-27 20:05:09 -0500

    added mobile responisiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-animate();
+// animate();
 
-function animate() {
-    
-}
+const c1 = document.querySelector('.bg-color1');
+const c2 = document.querySelector('.bg-color2');
+
+// function animate() {
+//     for (let i = 0; i < 10; i++) {
+        
+        
+        
+//     }
+// }
